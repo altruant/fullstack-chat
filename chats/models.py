4ba290c25c86281573ee2b71e00d254532dc33fb
altruant <noellehamberis@gmail.com>
2020-10-20 10:29:30 -0400

    made migrations and added chats model and urls

--- chats/models.py
+++ chats/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.conf import settings
+from django.utils import timezone
 
 # Create your models here.
+class Message(models.Model):
+
+    content = models.CharField(max_length=255)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.content
