9e4f112fcfa36f59f755028d90481a8ede9fd5c1
altruant <noellehamberis@gmail.com>
2020-10-20 10:44:01 -0400

    chat views and api endpoint

---
+++ chats/serializers.py
@@ -0,0 +1,11 @@
+from rest_framework import serializers
+
+
+from .models import Message
+
+class MessageSerializer(serializers.ModelSerializer):
+    user = serializers.ReadOnlyField(source='user.username')
+
+    class Meta:
+        model = Message
+        fields = ('id', 'content', 'user')
